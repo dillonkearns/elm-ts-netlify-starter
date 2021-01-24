1d41690a772966692baaeffcfc69f4d5216f4132
Dillon Kearns <dillon@dillonkearns.com>
2021-01-23 20:55:53 -0800

    Update generated code.

--- functions/elm-functions/schema.js
+++ functions/elm-functions/schema.js
@@ -2,25 +2,44 @@
 /** @typedef {import('ajv').JSONSchemaType<any>} JsonSchema */
 /** @type {{ [key: string]: { request: JsonSchema; response: JsonSchema; }}} */
 module.exports = {
-    hello: {
-        request: {
-            type: 'object',
-            properties: {
-                name: {
-                    type: 'string'
-                }
-            },
-            required: ['name']
-
-        }, response: {
-            type: 'object',
-            properties: {
-                message: {
-                    type: 'string'
-                    }
-            },
-            required: ['message']
-        }
-
+  "hello": {"request":{
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
     }
+  },
+  "required": [
+    "name"
+  ]
+},"response":{
+  "type": "object",
+  "properties": {
+    "message": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "message"
+  ]
+}},
+  "weather": {"request":{
+  "type": "object",
+  "properties": {
+    "unit": "unhandled"
+  },
+  "required": [
+    "unit"
+  ]
+},"response":{
+  "type": "object",
+  "properties": {
+    "degrees": {
+      "type": "integer"
+    }
+  },
+  "required": [
+    "degrees"
+  ]
+}}
 }
