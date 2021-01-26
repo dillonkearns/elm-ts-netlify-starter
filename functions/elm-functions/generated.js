c48ce17068bfa2142dc76394877d7c9f3bd3cd8e
Dillon Kearns <dillon@dillonkearns.com>
2021-01-25 18:22:46 -0800

    Extract errorHandler function.

--- functions/elm-functions/generated.js
+++ functions/elm-functions/generated.js
@@ -18,6 +18,63 @@ function getHandler(handlers)
   return ( (event, context) => handle(handlers, event, context))
 }
 
+
+/** @typedef {{kind: 'NoMsgParam'} | {kind: 'NoMsgHandler'; msgName: string;} | {kind: 'RequestBodyValidationFailed'; msg: string; errors: import("ajv").ErrorObject<string, Record<string, any>, unknown>[] | null | undefined} | {kind: 'ResponseBodyValidationFailed';errors: import("ajv").ErrorObject<string, Record<string, any>, unknown>[] | null | undefined} | {kind: 'MissingMsgHandler'; msgName: string;}} MsgError */
+
+/**
+ * @param {MsgError} error
+ * @returns {Promise<AWSLambda.APIGatewayProxyStructuredResultV2>}
+ */
+const errorHandler =
+ async function(error) {
+   switch (error.kind) {
+     case "NoMsgParam": {
+      return {
+          statusCode: 400,
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({errorMessage: "I expected a query parameter named 'msg'."})
+      }
+    }
+    case "NoMsgHandler": {
+      return {
+        statusCode: 404,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          errorMessage: `Function for msg '${error.msgName}' not found.`,
+        }),
+      };
+    }
+    case "RequestBodyValidationFailed": {
+      return {
+        statusCode: 422,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ errorMessage: error.errors }),
+      };
+    }
+    case "ResponseBodyValidationFailed": {
+      return {
+        statusCode: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ errorMessage: error.errors }),
+      };
+    }
+    case "MissingMsgHandler": {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          errorMessage: `Couldn't find a msg handler for ${error.msgName}.`,
+        }),
+      };
+    }
+   }
+}
+
 /**
  * @param {import("./generated").MsgHandlers} handlers
  * @param {import("./generated").APIGatewayProxyEvent} event
@@ -28,26 +85,14 @@ async function handle(handlers, event, context) {
   const msg = event.queryStringParameters?.msg;
   const parsedEventBody = event.body && JSON.parse(event.body)
   if (!msg) {
-    return {
-        statusCode: 400,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({errorMessage: "I expected a query parameter named 'msg'."})
-    }
+    return await errorHandler({kind: 'NoMsgParam'})
   }
   /** @type {import("./generated").MsgHandler<any, unknown> | null} */
   // @ts-ignore
   const msgHandler = msg && msg in handlers && handlers[msg];
   const msgValidator = msg && msg in validators && validators[msg]
   if (!msgValidator) {
-    return {
-        statusCode: 404,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({errorMessage: `Function for msg '${msg}' not found.`})
-    }
+    return await errorHandler({kind: 'NoMsgHandler', msgName: msg})
   }
   if (msgHandler) {
     const validateRequest = ajv.compile(msgValidator.request)
@@ -58,33 +103,22 @@ async function handle(handlers, event, context) {
         body: parsedEventBody,
       };
       // @ts-ignore
-      const rawResponse = await msgHandler(validatedEvent);
-      return responseToJson(ajv.compile(msgValidator.response), rawResponse);
+      const rawResponse = await msgHandler(validatedEvent, context);
+      return await responseToJson(ajv.compile(msgValidator.response), rawResponse);
     } else {
-      return {
-        statusCode: 422,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({errorMessage: validateRequest.errors})
-      }
+      return await errorHandler({kind: 'RequestBodyValidationFailed', msg: msg, errors: validateRequest.errors})
     }
   } else {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        errorMessage: `Couldn't find a msg handler for ${msg}.`,
-      }),
-    };
+      return await errorHandler({kind: 'MissingMsgHandler', msgName: msg})
   }
 }
 
 /**
  * @param {import("./generated").Response<unknown>} response
- * @returns {import("./generated").APIGatewayProxyStructuredResultV2}
  * @param {import("ajv").ValidateFunction<unknown>} validateResponse
+ * @returns {Promise<import("./generated").APIGatewayProxyStructuredResultV2>}
  */
-function responseToJson(validateResponse, response) {
+async function responseToJson(validateResponse, response) {
   const responseBody = response.body
   const validatedResponseBody = validateResponse(responseBody)
   console.log({validatedResponseBody, parsedResponseBody: responseBody});
@@ -95,14 +129,6 @@ function responseToJson(validateResponse, response) {
     };
     return { ...response, headers, body: JSON.stringify(responseBody) };
   } else {
-
-
-      return {
-        statusCode: 500,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({errorMessage: validateResponse.errors})
-      }
+      return await errorHandler({kind: 'ResponseBodyValidationFailed', errors: validateResponse.errors})
   }
 }
